838c2c42f116f5d1877c4ab2005676e3223406bc
Stephen Wang <swang.2420@gmail.com>
2025-05-12 17:22:11 -0400

    May 25 refactoring + cleanup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import Footer from '~/components/Footer';
 import MainNav from '~/components/MainNav';
+import { introData } from '~/data/websiteData';
 
 export default function Index() {
   return (
@@ -11,30 +12,22 @@ export default function Index() {
                 <div className="abt-img-container">
 					<img src="styles/img/Photo/Intro/stephen.jpeg" alt="Stephen Wang"></img>
 				</div>
-                <h1 className="abt-cap js--wp-1">Hi, I'm Stephen!</h1>     
+                <h1 className="abt-cap js--wp-1">{introData.photoIntro}</h1>     
             </div>
         </section> 
 
 		<section className="section-bio js--section-bio" id="bio">
             <div className="row">
                 <h2><b>About Me</b></h2>
-                
 				<div className="bio-paragraph">
-				<p className="long-copy">
-				I'm an incoming associate technical consultant at Appian with diverse technical interests. I am excited to bring applications to life using my knowledge in machine learning, data analytics, and full-stack web development. 
-				
-				In 2024, I graduated from Dartmouth College with a double major in Computer Science and Economics. During my free time, I might be learning a new piece on the piano, taking photos of stunning sunsets, skiing in on the Ice Coast, or striving someday to hit tennis balls at 0.1% the level of my commencement speaker. 
-
-				Feel free to reach me at swang.2420@gmail.com, I'm always open to chat about new opportunities!
-				</p>
+				  <p className="long-copy">{introData.indexIntro}</p>
 				</div>
-				
 				<div className="bio-paragraph">
 				<p className="long-copy">
 				<b> My Bucket List </b>
-					<li> See Wimbledon or the French Open in person </li>
-					<li> Go on a skiing trip in the Alps </li>
-					<li> Learn all of Chopin's 4 Ballades and 24 Etudes. I'm getting there :) </li>
+				  {introData.bucketList.map((item, index) => (
+					<li key={index}>{item}</li>
+				  ))}
 				</p>
 				</div>
             </div>
--- app/routes/piano.tsx
+++ app/routes/piano.tsx
@@ -1,27 +1,21 @@
 import Footer from '~/components/Footer';
 import MainNav from '~/components/MainNav';
 import Video from '~/components/Video';
+import { pianoData } from '~/data/websiteData';
 
 export default function piano() {
   return (
 	<body className="personal-slide">
 		<MainNav />
 		<header className="piano-bg"></header>
-
 		<section className="section-piano-vids js--section-features" id="features">
             <div className="row">
                 <h2><b>Listen</b></h2>
-				<Video title="Ballade No. 3 in A-Flat Major, Op. 47 - Chopin"
-				       vidLink="https://www.youtube.com/embed/zrW9H_q8lVM"/>
-
-				<Video title="Piano Sonata No. 21, Op. 53 'Waldstein' - Beethoven"
-				       vidLink="https://www.youtube.com/embed/YqGmNKT7LvY"/>
-				
-				<Video title="La Campanella - Liszt"
-				       vidLink="https://www.youtube.com/embed/BztamZSvGhQ"/>
-				
-				<Video title="Rondo in A-Minor, Op.59 - Kabalevsky"
-				       vidLink="https://www.youtube.com/embed/HTclVGjA0YA"/>
+				{
+				  pianoData.pieces.map((item, index) =>
+					<Video key={`piano-pieces-${index}`} title={item.title} vidLink={item.vidLink}/>
+				  )
+				}
             </div>
     	</section>
 
@@ -29,21 +23,18 @@ export default function piano() {
 			<div className="row">
 				<h2><b>Music Bio</b></h2>
 				<div className="bio-paragraph">
-				<p className="long-copy">
-				<b>Stephen Wang</b> started studying piano at age 6 under Shuefan Cheng and Evelyn Zuckerman, and has been a student of Mila Filatova since age 8. Stephen has been a prizewinner of international, division, and state competitions since age 9. He claimed 3rd place at the 10th Chopin International Piano Competition in Hartford, CT among 76 competitors representing 14 countries. He received 1st place at the New England Piano Teachers' Association Ruth Davidson Competition (for 9-10th grade), and 1st place at the Alice Hamlet Senior Scholarship Competition (for 11-12th grade) with the Margaret C. Bachelder Romantic Music Award. He awarded 1st place in the junior division and 2nd  place in the senior division of the Steinway Society Piano Competition for New England. From 2015 to 2019, Stephen has won five consecutive 1st places at the state level in the Music Teacher National Association piano performance competition, and was awarded 2nd place in the Eastern Division piano performance competition, where he competed against champions from 12 other contesting states. Stephen has also been a consecutive 1st place winner of the Granite State Piano Competition since 2011.  
-				</p>
+				  <p className="long-copy"> 
+					<b>{pianoData.bio.highlight}</b> 
+					{pianoData.bio.achievements} 
+				  </p>
 				</div>
 
 				<div className="bio-paragraph">
-				<p className="long-copy">
-				Stephen has participated in prestigious music festivals including the Music Fest Perugia in Italy, the Walnut Hill Music Festival in Boston, and the International Keyboard Institute and Festival in New York City. At age 10, Stephen was invited to Italy to play Bach's Piano Concerto in F Minor with the Parlarte Symphonic Orchestra. Since then, he has played Haydn's Piano Concerto in D Major, Mozart's Piano Concerto in G Major, and Beethoven's Concerto in C Minor with the Chamber Orchestra of Boston, the Nashua Chamber Orchestra, and the Sempre Musick Orchestra. Stephen enjoyed his appearances in unique venues such as Carnegie Hall, Boston Symphony Hall, Boston Conservatory, Mount Washington Resort, Dartmouth College Hopkins Center for the Arts, Dartmouth Hitchcock Medical Center, and Vermont Chandler Music Hall.
-				</p>
+				  <p className="long-copy"> {pianoData.bio.venues} </p>
 				</div>
 
 				<div className="bio-paragraph">
-				<p className="long-copy">
-				Stephen's love of music expands to chamber music and composition as well. His successful maiden work earned him 2nd place at the 2019 Granite State Composition Competition. In 10th grade, he created a music club and composed for its chamber group. With two other chamber groups, he has played Haydn's Piano Trio No. 39 in G Major (“Gypsy”), Dvorak's Piano Trio Op. 90 (“Dumky”), and Schumann's Piano Quintet Op. 44. Stephen has always been enthusiastic about sharing music with his community, and started performing regularly in hospitals, retirement centers, and churches in New England since age 8. He inspires and leads his music club members to also serve in the community and spread a passion for music.
-				</p>
+				  <p className="long-copy"> {pianoData.bio.community} </p>
 				</div>
 			</div>
 		</section>
